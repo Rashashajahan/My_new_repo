0fdeaa382f19d4d9b16f132d35eb6c4eb6df4b81
Rasha Shajahan <75829999+Rashashajahan@users.noreply.github.com>
2023-01-10 12:22:49 +0530

    Addition function

--- user_calculator.py
+++ user_calculator.py
@@ -1,4 +1,16 @@
-n=int(input("enter the first number: "))
-m=int(input("enter the second number: "))
-Addition=n+m
-print("sum of two number:", Addition)
+def add_two_num(a,b):
+    print("ADDITION FUNCTION")
+    try:
+        return a+b
+    except ValueError as val2:
+        val2=str(val2)
+        return str(val2)
+    except TypeError as val3:
+        val3=str(val3)
+        return val3
+    except NameError as val1:
+        val1=str(val1)
+        return str(val1)
+print(add_two_num(2,87))
+print(add_two_num(2,3.4))
+print(add_two_num(2,"hii"))
